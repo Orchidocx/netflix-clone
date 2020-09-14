493d7c1dde6450f1d1c83179f59a0a21277c57c9
Jacky Li <ilikemacaronii@gmail.com>
2020-09-14 12:29:06 -0700

    fix endpoints; useEffect to request data from api

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import axios from './axios';
 import './Row.css';
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    
-  }, []);
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
   return (
     <div className='row'>
--- src/requests.js
+++ src/requests.js
@@ -2,9 +2,9 @@
 const API_KEY = '25d56242b97872e3499cb76bcdbdae2c';
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_keys=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_keys=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_keys=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
