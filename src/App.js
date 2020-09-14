3e9b832a9d73a3cb595caebbe72d8bfbd2fd1841
Jacky Li <ilikemacaronii@gmail.com>
2020-09-14 13:50:02 -0700

    include banner component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
+      {/* Nav */}
+      <Banner/>
       <Row isLargeRow title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
       <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
