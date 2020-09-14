9fea5b0ec3f4e89cbd86b8a30d7f1546af6211ad
Jacky Li <ilikemacaronii@gmail.com>
2020-09-14 12:10:27 -0700

    attach requests object to Row prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <Row title='Netflix Originals'/>
-      <Row title='Trending'/>
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
