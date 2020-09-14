4cb05b299b597baca4c23e2bdb0064aefac5dd4c
Jacky Li <ilikemacaronii@gmail.com>
2020-09-14 12:13:06 -0700

    export default requests; add useEffect

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Row.css';
 
 function Row({title}) {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    
+  }, []);
+
   return (
     <div className='row'>
       <h2>{title}</h2>
